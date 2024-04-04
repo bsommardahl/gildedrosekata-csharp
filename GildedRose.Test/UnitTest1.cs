aaae8a2b50dd2950da448db7e645b72f19d8d39c
Byron Sommardahl <byron@sommardahl.com>
2024-04-04 11:25:57 -0500

    Adding the initial solution.

---
+++ GildedRose.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace GildedRose.msTest;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ GildedRose/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace GildedRose;
+
+public class Class1
+{
+}
